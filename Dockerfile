8f7f8c5f74a1ea8f35aa3b2cef74ad11d1b531ea
Christian Dupuis <cd@atomist.com>
2021-09-09 16:54:06 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+ARG VERSION=0.0.0
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS restore
+ARG VERSION
+WORKDIR /
+
+COPY ./nuget.config .
+COPY ./*.sln .
+COPY ./Directory.Build.props .
+COPY ./src/Kevsoft.WLED/*.csproj ./src/Kevsoft.WLED/
+COPY ./test/Kevsoft.WLED.Tests/*.csproj ./test/Kevsoft.WLED.Tests/
+RUN dotnet restore
+
+FROM restore as build
+ARG VERSION
+COPY ./icon.png .
+COPY ./src/Kevsoft.WLED/ ./src/Kevsoft.WLED/
+RUN dotnet build ./src/**/*.csproj --configuration Release -p:Version=${VERSION} --no-restore
+
+FROM build as build-tests
+ARG VERSION
+COPY ./test/Kevsoft.WLED.Tests/ ./test/Kevsoft.WLED.Tests/
+RUN dotnet build ./test/**/*.csproj --configuration Release -p:Version=${VERSION} --no-restore
+
+FROM build-tests as test
+ENTRYPOINT ["dotnet", "test", "./test/Kevsoft.WLED.Tests/Kevsoft.WLED.Tests.csproj", "--configuration", "Release", "--no-restore", "--no-build"]
+CMD ["--logger" , "trx", "--results-directory", "./TestResults"]
+
+FROM build as pack
+ARG VERSION
+RUN dotnet pack --configuration Release -p:Version=${VERSION} --no-build
+
+FROM pack as push
+RUN env
+
+COPY --from=restore ./src/Kevsoft.WLED/packages.lock.json ./src/Kevsoft.WLED/packages.lock.json
+
+ENTRYPOINT ["dotnet", "nuget", "push", "./src/Kevsoft.WLED/bin/Release/*.nupkg", "--source", "NuGet.org"]
---
+++ src/Kevsoft.WLED/WLedClient.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Kevsoft.WLED
+{
+    public class WLedClient
+    {
+        private readonly HttpClient _client;
+
+        public WLedClient(HttpMessageHandler httpMessageHandler, string baseUri)
+        {
+            _client = new HttpClient(httpMessageHandler)
+            {
+                BaseAddress = new Uri(baseUri, UriKind.Absolute),
+            };
+        }
+
+        public WLedClient(string baseUri) : this(new HttpClientHandler(), baseUri)
+        {
+
+        }
+
+        public async Task<WLedRootResponse> Get()
+        {
+            var message = await _client.GetAsync("json");
+
+            message.EnsureSuccessStatusCode();
+
+            var response = await JsonSerializer.DeserializeAsync<WLedRootResponse>(await message.Content.ReadAsStreamAsync());
+
+            return response;
+        }
+
+        public async Task<State> GetState()
+        {
+            var message = await _client.GetAsync("json/state");
+
+            message.EnsureSuccessStatusCode();
+
+            var response = await JsonSerializer.DeserializeAsync<State>(await message.Content.ReadAsStreamAsync());
+
+            return response;
+        }
+
+        public async Task<Information> GetInformation()
+        {
+            var message = await _client.GetAsync("json/info");
+
+            message.EnsureSuccessStatusCode();
+
+            var response = await JsonSerializer.DeserializeAsync<Information>(await message.Content.ReadAsStreamAsync());
+
+            return response;
+        }
+
+        public async Task<string[]> GetEffects()
+        {
+            var message = await _client.GetAsync("json/eff");
+
+            message.EnsureSuccessStatusCode();
+
+            var response = await JsonSerializer.DeserializeAsync<string[]>(await message.Content.ReadAsStreamAsync());
+
+            return response;
+        }
+
+        public async Task<string[]> GetPalettes()
+        {
+            var message = await _client.GetAsync("json/pal");
+
+            message.EnsureSuccessStatusCode();
+
+            var response = await JsonSerializer.DeserializeAsync<string[]>(await message.Content.ReadAsStreamAsync());
+
+            return response;
+        }
+    }
+}
